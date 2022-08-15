3ae498771c422937489f916dadf1ef8f18ea5132
cupcakestudios <coolting.lin3@gmail.com>
2022-08-15 16:49:10 +0200

    øvelse 3 - gæt tal done

---
+++ oevelser3.js
@@ -0,0 +1,35 @@
+  
+
+//fandt via MDN  docs
+ function getRandomIntInclusive(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(10);
+  return Math.floor(Math.random() * (10 - 0 + 1) + 0); //The maximum is inclusive and the minimum is inclusive
+}
+
+ var randomNumber = getRandomIntInclusive(0,10); //generer tilfældigt tal
+
+
+  const guess = document.querySelector("button");
+  guess.addEventListener('click', guessNumber);
+ //en gætte-tæller, tæller hvor mange gange man har forsøgt
+  var guessCounter = 0;
+  guess.onclick = () => guessCounter++;
+  
+
+function guessNumber()
+ {
+   if (randomNumber == document.querySelector("#tal").value) 
+  {
+    alert(document.querySelector("#tal").value +" er rigtigt! Du brugte " + guessCounter + " forsøg. Prøv igen med et nyt tal?");
+  }
+  else if (document.querySelector("#tal").value > randomNumber)
+  {
+    alert("Øv, " + document.querySelector("#tal").value +" var for højt");
+  }
+  else 
+  {
+    alert("Øv, " + document.querySelector("#tal").value +" var for lavt");
+  }
+  
+  }
